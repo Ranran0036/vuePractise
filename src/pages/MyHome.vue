6b2b625e6789d87adb11fd594781f3789a9653de
Ranran <ranran0036@163.com>
2022-04-07 13:19:58 +0800

    solve  : parrent route lose active_2

--- src/pages/MyHome.vue
+++ src/pages/MyHome.vue
@@ -6,11 +6,11 @@
         <div>
             <ul class="nav nav-tabs">
                 <li>
-                    <router-link class="list-group-item" active-class="active" to="/MyHome/homenews">News</router-link>
+                    <router-link class="list-group-item" active-class="active" to="/MyHome/HomeNews">News</router-link>
                 </li>
                 <li>
                     <!-- to路径大小写不一致虽然不导致跳转bug 但是会出现点击子路由 父路由失去active样式 -->
-                    <router-link class="list-group-item" active-class="active" to="/MyHome/homemessage">Message</router-link>
+                    <router-link class="list-group-item" active-class="active" to="/MyHome/HomeMessage">Message</router-link>
                 </li>
             </ul>
             <router-view></router-view>
